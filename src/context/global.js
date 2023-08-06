c3aeb6058b4f0e271ffcd9cc4fcb737ca0ef3a95
Ali Nabilah Ramadhan <ali.nabilah@quatronema.co.id>
2023-08-06 19:08:33 +0700

    add initial state

--- src/context/global.js
+++ src/context/global.js
@@ -3,8 +3,22 @@ import React, { createContext, useContext } from 'react'
 const GlobalContext = createContext();
 
 export const GlobalContextProvider = ({children}) => {
+
+  // Initial state
+  const initialState = {
+    popularAnime: [],
+    upcomingAnime: [],
+    airingAnime: [],
+    pictures: [],
+    isSearch: false,
+    searchResults: [],
+    loading: false,
+  }
+
   return (
-    <GlobalContext.Provider value={'Hello'}>
+    <GlobalContext.Provider value={{
+
+    }}>
       {children}
     </GlobalContext.Provider>
   )
