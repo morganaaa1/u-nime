5eed81ce4e7f47af34cc44589c932fa9948231d0
Ali Nabilah Ramadhan <ali.nabilah@quatronema.co.id>
2023-08-06 20:58:48 +0700

    fixing ocsr fetch api

--- src/context/global.js
+++ src/context/global.js
@@ -1,50 +1,72 @@
-import React, { createContext, useContext, useEffect, useReducer } from 'react'
+import React, {createContext, useContext, useReducer} from "react";
 
 const GlobalContext = createContext();
 
+const baseUrl = "https://api.jikan.moe/v4";
+
+//actions
+const LOADING = "LOADING";
+const SEARCH = "SEARCH";
+const GET_POPULAR_ANIME = "GET_POPULAR_ANIME";
+const GET_UPCOMING_ANIME = "GET_UPCOMING_ANIME";
+const GET_AIRING_ANIME = "GET_AIRING_ANIME";
+const GET_PICTURES = "GET_PICTURES";
+
+//reducer
+const reducer = (state, action) => {
+    switch(action.type){
+        case LOADING:
+            return {...state, loading: true}
+        case GET_POPULAR_ANIME:
+            return {...state, popularAnime: action.payload, loading: false}
+        case SEARCH:
+            return {...state, searchResults: action.payload, loading: false}
+        case GET_UPCOMING_ANIME:
+            return {...state, upcomingAnime: action.payload, loading: false}
+        case GET_AIRING_ANIME:
+            return {...state, airingAnime: action.payload, loading: false}
+        case GET_PICTURES:
+            return {...state, pictures: action.payload, loading: false}
+        default:
+            return state;
+    }
+}
+
+
 export const GlobalContextProvider = ({children}) => {
+    //intial state
+    const intialState = {
+        popularAnime: [],
+        upcomingAnime: [],
+        airingAnime: [],
+        pictures: [],
+        isSearch: false,
+        searchResults: [],
+        loading: false,
+    }
 
-  const baseUrl = 'https://api.jikan.moe/v4'
+    const [state, dispatch] = useReducer(reducer, intialState);
 
-  // Reducer
-  const reducer = (state, action) => {
-    return state;
-  }
+    //fetch popular anime
+    const getPopularAnime = async () => {
+        const response = await fetch(`${baseUrl}/top/anime?filter=bypopularity`);
+        const data = await response.json();
+        console.log(data.data)
+    }
+    //initial render
+    React.useEffect(() => {
+        getPopularAnime();
+    }, [])
 
-  // Initial state
-  const initialState = {
-    popularAnime: [],
-    upcomingAnime: [],
-    airingAnime: [],
-    pictures: [],
-    isSearch: false,
-    searchResults: [],
-    loading: false,
-  }
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  // Fetch Popular Anime
-  const getPopularAnime = async () => {
-    const response = await fetch(`${baseUrl}/top/anime/?filter=bypopularity`);
-    const data = await response.json();
-    console.log(data.data);
-  }
-
-  // initial render
-  useEffect(() => {
-    getPopularAnime();
-  }, [])
-
-  return (
-    <GlobalContext.Provider value={{
-      ...state,
-    }}>
-      {children}
-    </GlobalContext.Provider>
-  )
+    return(
+        <GlobalContext.Provider value={{
+            ...state,
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
 }
 
 export const useGlobalContext = () => {
-  return useContext(GlobalContext)
+    return useContext(GlobalContext);
 }
